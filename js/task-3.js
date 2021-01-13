4d7300cbe6ef6065e74a99af5e251bb71a6a0fc2
Klem26 <Kankina26@gmail.com>
2021-01-13 18:39:19 +0200

    код задания

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+const apartment = {
+    imgUrl: 'https://via.placeholder.com/640x480',
+    descr: 'Просторная квартира в центре',
+    rating: 4,
+    price: 2153,
+    tags: ['premium', 'promoted', 'top'],
+  };
+  
+  
+   const aptRating = apartment.rating;
+  const aptDescr = apartment.descr;
+  const aptPrice = apartment.price;
+const aptTags = apartment.tags;
+  
+//выражения обращения к соответствующим свойствам обьекта apartment.
